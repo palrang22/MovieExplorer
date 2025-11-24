85b1a0aa3ad6c4312c0ec1e76db64cdfecacce4b
palrang22 <kseunghui@gmail.com>
2025-11-24 16:06:50 +0900

    Initial Commit

---
+++ MovieExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieExplorer
+//
+//  Created by 김승희 on 11/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
